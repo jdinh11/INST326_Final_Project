a0440089c5b26eacac262603a88887420d1b7d75
jdinh11 <jaden.dinh02@gmail.com>
2023-04-19 22:12:26 -0400

    Create recommender.py
    Set up the functions and docstrings

---
+++ recommender.py
@@ -0,0 +1,95 @@
+class Movie():
+    """A class for storing information regarding a list of Netflix medias
+
+    Attributes:
+        title (str): the media's title
+        type (str): the type of media (movie or show)
+        gernes (list): the media's list of associated gernes
+        age_ratings (str): the media's age restrictions (if any)
+        imdb_score (int): the media's imdb ratings 
+    """
+    def __init__(self, title, type, gernes, age_rating, imdb_score):
+        """Initializes a Movie class
+
+        Args:
+        """
+        pass
+    def get_movieID(path):
+        """Get the media's ID on the dataset
+        
+        Args:
+            path: the path to the dataset
+        """
+        pass
+
+    def get_name(path):
+        """Get the media's name
+        
+        Args:
+            path: the path to the dataset
+        """
+        pass
+    
+    def get_media_type(path):
+        """Get the media's type
+        
+        Args: 
+            path: the path to the dataset
+        """
+        pass
+
+    def get_genre(path):
+        """Get the media's gerne(s)
+        
+        Args:
+            path: the path to the dataset
+        """
+        pass
+
+    def get_age_rating(path):
+        """Get the media's age restrictions
+        
+        Args:
+            path: the path to the dataset
+        """
+        pass
+
+    def get_imdb_score(path):
+        """Get the media's imbd ratings
+        
+        Args:
+            path: the path to the dataset
+        """
+        pass
+
+    def get_recommendations(input, ):
+        """Get recommendations based on friend's favorite movies
+
+        Args: 
+            input (list)
+        """
+        pass
+
+def check_movie(input):
+    """Check if the user's movies exists within the dataset
+
+    Args: 
+        input (string): the movie's name
+
+    Return:
+        the movie's name if it passes the check
+
+    Raise Error:
+        Error: the movie does not exist within the dataset
+    """
+    pass
+
+def format_results(recommendations):
+    """Format the recommendation list for the user
+    Args:
+        recommedations (list): the list recommended movies
+    
+    Return:
+        the recommendation displayed in the formatted structure
+    """
+    pass
