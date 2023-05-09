bcd3e5b9512be77ff7111aebaa90fe4240bb081c
kevinkn1902 <kevinplaysbaseball1902@gmail.com>
2023-05-08 21:34:33 -0400

    recommender_test update

--- recommender_test.py
+++ recommender_test.py
@@ -6,4 +6,4 @@ def database():
     return Database("titles.csv")
 
 def movie():
-    return Movie("tm70993", "Life of Brian", "MOVIE", "Brian Cohen is an average young Jewish man, but through a series of ridiculous events, he gains a reputation as the Messiah. When he's not dodging his followers or being scolded by his shrill mother, the hapless Brian has to contend with the pompous Pontius Pilate and acronym-obsessed members of a separatist movement. Rife with Monty Python's signature absurdity, the tale finds Brian's life paralleling Biblical lore, albeit with many more laughs.", ['comedy','GB'], "R")
+    return Movie("tm70993", "Life of Brian", "MOVIE", "Brian Cohen is an average young Jewish man, but through a series of ridiculous events, he gains a reputation as the Messiah. When he's not dodging his followers or being scolded by his shrill mother, the hapless Brian has to contend with the pompous Pontius Pilate and acronym-obsessed members of a separatist movement. Rife with Monty Python's signature absurdity, the tale finds Brian's life paralleling Biblical lore, albeit with many more laughs.", ['comedy','GB'], "R", 7.8)
