643efd07d05c5107570c28cd24bc24a0bd2f5d1d
Megan Cha <mcha9103@terpmail.umd.edu>
2023-05-16 15:39:58 -0400

    attempt 2 of updated user test

--- test_recommender.py
+++ test_recommender.py
@@ -104,6 +104,6 @@ def test_User():
     preferences_obj = User(preferences)
     #turns the name attribute in the User object into a string
     str_name = str(user_obj.name)
-    #checks to see if the value in the attribute matches the name
+    #checks to see if the values in the attribute match the string (name) and list (preferences)
     assert str_name == 'Megan'
     assert preferences_obj == ['adventure', 'comedy', 'action']
